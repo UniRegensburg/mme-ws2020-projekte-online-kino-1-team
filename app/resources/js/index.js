58a272008eb1bff400929443a39737ebc9d9a2fd
Sandro <sandro.michl@web.de>
2021-02-21 20:25:14 +0100

    Dropdown Menü eingefügt; Overlay gelöscht

--- app/resources/js/index.js
+++ app/resources/js/index.js
@@ -1,7 +1,24 @@
 /* eslint-env browser */
 
+let joinRoomButton, closeDropDownMenu;
+
 function init() {
-	console.log(document.querySelector(".topBar"));
+	setClickListener();
+}
+
+function setClickListener(){
+	joinRoomButton = document.querySelector(".joiningRoom");
+	closeDropDownMenu = document.querySelector(".closeDropDownMenu");
+	joinRoomButton.addEventListener("click", showURLTextBox);
+	closeDropDownMenu.addEventListener("click", hideURLTextBox);
+}
+
+function showURLTextBox(){
+	document.querySelector(".dropDownMenu").classList.remove("hidden");
+}
+
+function hideURLTextBox(){
+	document.querySelector(".dropDownMenu").classList.add("hidden");
 }
 
 init();
