7b8c34e84e7419cb2f1d059dfbd3d2eb9759aab5
JKellnhofer <jakob.kellnhofer@stud.uni-regensburg.de>
2021-03-04 12:28:39 +0100

    added cors-options for socket.io

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 //Server
 
-
 var server,
   rooms;
 
@@ -10,7 +9,7 @@ const AppServer = require("./server/AppServer.js"),
   express = require("express"),
 
   httpServer = require("http").createServer(express),
-  options = {},
+  options = {credentials: true, origin: "http:localhost:3000"},
   io = require("socket.io")(httpServer, options);
 
 io.on("connection", (socket) => {
@@ -18,30 +17,32 @@ io.on("connection", (socket) => {
 
   //Hier kommen alle Callbacks für Server-Client Communikation rein:
 
-  socket.on("createRoom", date => createRoom(date));
-  socket.on("date", date => {
+  socket.on("createRoom", (date) => createRoom(date));
+  socket.on("date", (date) => {
     console.log(date);
   });
 
 });
 
 httpServer.listen(3000, function() {
+
+
   console.log("Ich höre auf socket IO Port 3000");
 });
 
-function createRoom(roomName){
-    console.log("Create new Room" + roomName);
+function createRoom(roomName) {
+  console.log("Create new Room" + roomName);
 }
 /**
  * Starts webserver to serve files from "/app" folder
  */
 function init() {
-    if (rooms === null){
-        this.rooms = {};
-    }
-    else {
-        this.rooms = rooms; 
-    }
+
+  if (rooms === null) {
+    this.rooms = {};
+  } else {
+    this.rooms = rooms;
+  }
   // Access command line parameters from start command (see package.json)
   let appDirectory = process.argv[2], // folder with client files
     appPort = process.argv[3]; // port to use for serving static files
