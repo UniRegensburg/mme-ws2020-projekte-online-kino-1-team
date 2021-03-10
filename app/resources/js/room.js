5988f62e75913dfdc0af358b87a16a9a47970dad
Niklas Keller <niklaskel99@gmail.com>
2021-03-10 13:39:03 +0100

    ShowMessages
    Now able to see what you write in chat

--- app/resources/js/room.js
+++ app/resources/js/room.js
@@ -1,14 +1,21 @@
-let nicknameTextField;
+let nicknameTextField,
+messageTextField;
 
 function init(){
     setClickListener();
 }
 
 function setClickListener(){
+    messageTextField = document.querySelector(".typeField");
     nicknameTextField = document.querySelector(".nickname");
     nicknameTextField.addEventListener("keypress", function (e){
         if(e.key === "Enter" && nicknameTextField.value !== ""){
             enterNickname();
+        }
+    });
+    messageTextField.addEventListener("keypress", function (e){
+        if(e.key === "Enter" && messageTextField.value !== ""){
+            addMessage();
         }
     });
 }
@@ -17,6 +24,13 @@ function enterNickname(){
     nicknameTextField.classList.add("hidden");
     document.querySelector(".chat").classList.remove("disabled");
     document.querySelector(".typeField").disabled = false;
+}
+
+function addMessage(){
+    var chat = document.querySelector(".chat"),
+    message = document.createElement("div");
+    message.innerHTML = messageTextField.value;
+    chat.insertBefore(message, document.querySelector(".typeField"));
 }
 
 init();
