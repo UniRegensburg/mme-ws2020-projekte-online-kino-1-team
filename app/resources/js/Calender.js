7707e7de75fa923a3addb4b90b6e2d027cdf3ed2
Sandro <sandro.michl@web.de>
2021-03-11 15:24:05 +0100

    ICal Download fertig

--- app/resources/js/Calender.js
+++ app/resources/js/Calender.js
@@ -1,3 +1,5 @@
+import {CAL_SINGLE} from "./createICal.js";
+
 class Calender {
 
     clickListenerCalender() {
@@ -6,14 +8,17 @@ class Calender {
             submitButton = document.querySelector(".overlay-submit-button"),
             date = document.querySelector(".overlay-choose-date"),
             time = document.querySelector(".overlay-choose-time"),
-            error = document.querySelector(".overlay-input-error");
+            error = document.querySelector(".overlay-input-error"),
+            overlayOutputLink = document.querySelector(".overlay-output-link");
         calenderButton.addEventListener("click", openLayout);
         closeButton.addEventListener("click", closeOverlay);
-        submitButton.addEventListener("click", function(){
-            if(date.value !== "" && time.value !== ""){
+        submitButton.addEventListener("click", function () {
+            if (date.value !== "" && time.value !== "") {
                 readFormInput();
-                error.classList.remove("active");                
-            }else{
+                error.classList.remove("active");
+                addICalInCalender();
+                overlayOutputLink.classList.add("active");
+            } else {
                 error.classList.add("active");
             }
         });
@@ -21,7 +26,9 @@ class Calender {
 }
 
 let overlay = document.querySelector(".overlay"),
-    overlayBackground = document.querySelector(".overlay-background");
+    overlayBackground = document.querySelector(".overlay-background"),
+    date = document.querySelector(".overlay-choose-date"),
+    time = document.querySelector(".overlay-choose-time");
 
 function openLayout() {
     overlay.classList.add("active");
@@ -34,13 +41,18 @@ function closeOverlay() {
 }
 
 function readFormInput() {
-    let date = document.querySelector(".overlay-choose-date"),
-    time = document.querySelector(".overlay-choose-time"),
-    overlayOutput = document.querySelector(".overlay-output");
-    overlayOutput.classList.add("active");
-    overlayOutput.innerHTML = "Der Raum ist reserviert für den " + date.value + " um " + time.value + " Uhr. <br>" + 
-        "Unter der folgenden Url kannst du schon einmal beitreten und alles einstellen: ?08748456148745189? <br><br>" + 
-        "Klicke hier, um den Termin in deinem Kalender zu speichern.";
+    let overlayOutputExplanation = document.querySelector(".overlay-output-explanation");
+    overlayOutputExplanation.classList.add("active");
+    overlayOutputExplanation.innerHTML = "Der Raum ist reserviert für den " + date.value + " um " + time.value + " Uhr. <br>" +
+        "Unter der folgenden Url kannst du schon einmal beitreten und alles einstellen: ?08748456148745189?";
+}
+
+function addICalInCalender() {
+    let dateAndTime = date.value + " " + time.value;
+    CAL_SINGLE.addEvent("WatchMates", "Du hast dir diese URL gesichert: ?50510113210471455121?", "", dateAndTime, "");
+    document.querySelector(".overlay-output-link").addEventListener("click", function () {
+        CAL_SINGLE.download("Termin WatchMates");
+    });
 }
 
 export { Calender };
