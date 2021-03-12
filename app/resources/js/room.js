6991cd39a61c1cded4c7af2d34710338e298ab54
Celina <c.kurrer@gmx.de>
2021-03-12 14:04:45 +0100

    Video starten mit der Video.js; Verschiedene Methoden für ein Video. Erste Playlist-Fassung.
    Co-authored-by: JKellnhofer <JKellnhofer@users.noreply.github.com>

--- app/resources/js/room.js
+++ app/resources/js/room.js
@@ -1,19 +1,44 @@
-let nicknameTextField;
+/* eslint-env node */
+import VideoPlayer from "./VideoPlayer.js";
 
-function init(){
+let nicknameTextField;
+var player;
+
+function init() {
+    let tempPlaylist = [],
+        temp2Playlist = [],
+        sources1 = {
+            sources: [{
+                src: "resources/css/Bunny.mp4",
+                type: "video/mp4",
+            }],
+        },
+        sources2 = {
+            sources: [{
+                src: "resources/css/Testing.mp4",
+                type: "video/mp4",
+            }],
+        };
+
+    tempPlaylist.push(sources1, sources2);
+    temp2Playlist.push(sources2, sources1);
+
+    // eslint-disable-next-line no-unused-vars
+    player = new VideoPlayer(tempPlaylist);
+
     setClickListener();
 }
 
-function setClickListener(){
+function setClickListener() {
     nicknameTextField = document.querySelector(".nickname");
-    nicknameTextField.addEventListener("keypress", function (e){
-        if(e.key === "Enter" && nicknameTextField.value !== ""){
+    nicknameTextField.addEventListener("keypress", function (e) {
+        if (e.key === "Enter" && nicknameTextField.value !== "") {
             enterNickname();
         }
     });
 }
 
-function enterNickname(){
+function enterNickname() {
     nicknameTextField.classList.add("hidden");
     document.querySelector(".chat").classList.remove("disabled");
     document.querySelector(".typeField").disabled = false;
